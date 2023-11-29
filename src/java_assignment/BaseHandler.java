5f32324f8caf7eae289e217beaadc54ce94b9a79
sharkie <sharkie@localhost>
2023-11-30 00:41:57 +0800

    added progress but just for trial

--- src/java_assignment/BaseHandler.java
+++ src/java_assignment/BaseHandler.java
@@ -1,17 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package java_assignment;
+
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 
-/**
- *
- * @author sharkie
- */
 public class BaseHandler<T extends IDataContainer> {
     
     public ArrayList<T> collection = new ArrayList<T>();
